5dcbda27345038819040af4993768f88d93980c4
Kenneth Kipkoech Sigei <KSIGEI@safaricom.co.ke>
2022-08-16 09:48:00 +0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import RMDBLogo from "../../images/react-movie-logo.svg";
+import TMDBLogo from "../../images/tmdb_logo.svg";
+
+import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
+
+const Header = () => (
+  <Wrapper>
+    <Content>
+      <Link to="/">
+        <LogoImg src={RMDBLogo} alt="rmdb-logo" />
+      </Link>
+      <LogoImg src={RMDBLogo} alt="rmdb-logo" />
+      <TMDBLogoImg src={TMDBLogo} alt="tmdb" />
+    </Content>
+  </Wrapper>
+);
+
+export default Header;
